b60f181deb2721a947023e06e9b1f35e3122342c
Cuascota <44456154+Cuascota@users.noreply.github.com>
2019-02-05 09:23:22 -0500

    Add files via upload

---
+++ Henry_Cuascota_ConfigurarSleep.ino
@@ -0,0 +1,36 @@
+/*
+ *      UNIVERSIDAD TECNICA DEL NORTE 
+ *          CIERCOM-FICA
+ *          
+ * NOMBRE: HENRY CUASCOTA
+ * TEMA:   configurar el sleep cada 25s
+ * 
+ */
+
+#include <LowPower.h>
+
+int levantar = 2;
+
+int cnt = 0;
+int num = cnt ;
+
+void setup(){  
+      Serial.begin(9600);
+      pinMode( levantar, INPUT);
+   }
+void loop(){   
+  
+    attachInterrupt( 0, configurar, FALLING);
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+    detachInterrupt(0); 
+       
+    if (num != cnt){
+      Serial.println(cnt);
+       num = cnt ;
+       Serial.flush();
+          }
+   }
+
+void configurar() {    
+  cnt++ ;
+   }
